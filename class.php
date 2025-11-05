cdb2a2cbfb6dc8e0d6a7c1bfa5449c2479635053
Roni Kurnia <roni.kurnia4249@gmail.com>
2025-11-05 19:00:35 +0700

    tipe data object

--- class.php
+++ class.php
@@ -1,15 +1,10 @@
 <?php
-
 class produk {
     public $judul, 
             $penulis,
             $penerbit,
             $harga;
-    
-    // method construct dijalankan setiap pemanggilan objek
-    // jadi tidak perlu untuk mendefinisikan property di objek lagi
-    // sebelumnya: produk1->judul = "naruto";
-    // memeberikan nilai default pada property
+
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis; 
@@ -17,21 +12,18 @@ class produk {
         $this->harga = $harga;
     }
 
-    // berfungsi untuk mengembalikan nilai dari value di objek yang menimpa property
-    public function label() {
-        return "$this->judul,
-        $this->penulis, 
-        $this->penerbit, 
-        $this->harga";
+    // membuat method untuk memanggil tipe data object(str)
+    // setiap class bisa dipanggil dari file lain
+    // method cetak dibatasi hannya dapat memanggil data dari class produk
+    public function cetak(produk $produkan) {
+        $str = "{$produkan->judul} | {$produkan->penulis}, {$produkan->penerbit} (Rp.{$produkan->harga})";
+        return $str;
     }
 }
 
-$produk1 = new produk("naruto", "Fujimoto", "jump", 30000);
-$produk2 = new produk("metal gear", "kojima", "konami", 23000);
-$produk3 = new produk("tales");
+$produk1 = new produk("Naruto", "Masashi kishimoto", "Shonen Jump", 30000);
+$produk2 = new produk("Metalgear", "Hideo Kojima", "konami", 230000);
 
-echo "komik: " . $produk1->label();
+echo "komik: " . $produk1->cetak($produk1);
 echo "<br>";
-echo "game : " . $produk2->label();
-echo "<br>";
-echo "game : " . $produk3->label();
+echo "game : " . $produk2->cetak($produk2);
