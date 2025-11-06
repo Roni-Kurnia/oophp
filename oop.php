ba9ab5c4e6f5cdf63e1b55cf110283c0d27aaa7a
Roni Kurnia <roni.kurnia4249@gmail.com>
2025-11-06 12:15:22 +0700

    overiding

--- oop.php
+++ oop.php
@@ -1,56 +1,61 @@
 <?php
-
-// ini class parent
 class produk {
     public $judul, 
             $penulis,
             $penerbit,
-            $harga,
-            $jmlHalaman,
-            $play;
+            $harga;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $play = 0) {
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis; 
         $this->penerbit = $penerbit; 
         $this->harga = $harga;
+    }
+
+    public function cetak() {
+        $str = "{$this->judul} | {$this->penulis}, {$this->penerbit} (Rp.{$this->harga})";
+        return $str;
+    }
+}
+
+// komik
+class komik extends produk {
+    public $jmlHalaman;
+
+    public function cetak() {
+        $str = "Komik: ". parent::cetak() ." - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
         $this->jmlHalaman = $jmlHalaman;
+    }
+}
+
+// game
+class game extends produk {
+    public $play;
+
+    public function cetak() {
+        $str = "Game: ". parent::cetak() ." - {$this->play} Jam.";
+        return $str;
+    }
+
+        public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $play = 0) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
         $this->play = $play;
     }
-
-    public function getLabel() {
-        $str = "{$this->penulis}, {$this->penerbit}";
-        return $str;
-    }
-
-    public function cetak() {
-        $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
-        return $str;
-    }
 }
 
-// ini class child
-class komik extends produk {
-    public function cetak() {
-        $str = "Komik: {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->jmlHalaman} Halaman.";
-        return $str;
-    }
-}
-
-// ini class child
-class game extends produk {
-    public function cetak() {
-        $str = "Game: {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->play} Jam.";
-        return $str;
-    }
-}
-
-$produk1 = new komik("Naruto", "Masashi kishimoto", "Shonen Jump", 30000, 100, 0);
-$produk2 = new game("Metalgear", "Hideo Kojima", "konami", 230000, 0, 50);
+$produk1 = new komik("Naruto", "Masashi kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new game("Metalgear", "Hideo Kojima", "konami", 230000, 50);
 
 echo $produk1->cetak($produk1);
 echo "<br>";
 echo $produk2->cetak($produk2);
 
-// class child akan mewarisi semua property dan method calss parent
-// class child dapat memperluas fungsionalitas dari class parent secara private
+// penggunaan parent::,method() adalah sdebagai overididng 
+// penggunaan :: pada overiding berperan sebagai tipe data static
