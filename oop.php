49c78da3a6f8968e08f1b3b78653067e8fb6cd3a
Roni Kurnia <roni.kurnia4249@gmail.com>
2025-11-06 11:19:23 +0700

    inheritance solution

--- oop.php
+++ oop.php
@@ -1,39 +1,56 @@
 <?php
+
+// ini class parent
 class produk {
     public $judul, 
             $penulis,
             $penerbit,
             $harga,
             $jmlHalaman,
-            $play,
-            $tipe;
+            $play;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $play = 0, $tipe) {
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $play = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis; 
         $this->penerbit = $penerbit; 
         $this->harga = $harga;
         $this->jmlHalaman = $jmlHalaman;
         $this->play = $play;
-        $this->tipe = $tipe;
     }
 
-    // menambahkan data "tipe" pada program 
-    // ini menjadi masalah jika konfigurasi menjadi sangat banyak akan menjadi tidak efisien
-    public function cetak(produk $produkan) {
-        $str = "{$produkan->tipe} : {$produkan->judul} | {$produkan->penulis}, {$produkan->penerbit} (Rp.{$produkan->harga})";
-        if ($this->tipe == "komik") {
-            $str .= " - {$this->jmlHalaman} Halaman.";
-        } else if ($this->tipe == "game") {
-            $str .= " - {$this->play} Jam.";
-        };
+    public function getLabel() {
+        $str = "{$this->penulis}, {$this->penerbit}";
+        return $str;
+    }
+
+    public function cetak() {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
         return $str;
     }
 }
 
-$produk1 = new produk("Naruto", "Masashi kishimoto", "Shonen Jump", 30000, 100, 0, "komik");
-$produk2 = new produk("Metalgear", "Hideo Kojima", "konami", 230000, 0, 50, "game");
+// ini class child
+class komik extends produk {
+    public function cetak() {
+        $str = "Komik: {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+// ini class child
+class game extends produk {
+    public function cetak() {
+        $str = "Game: {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) - {$this->play} Jam.";
+        return $str;
+    }
+}
+
+$produk1 = new komik("Naruto", "Masashi kishimoto", "Shonen Jump", 30000, 100, 0);
+$produk2 = new game("Metalgear", "Hideo Kojima", "konami", 230000, 0, 50);
 
 echo $produk1->cetak($produk1);
 echo "<br>";
 echo $produk2->cetak($produk2);
+
+// class child akan mewarisi semua property dan method calss parent
+// class child dapat memperluas fungsionalitas dari class parent secara private
